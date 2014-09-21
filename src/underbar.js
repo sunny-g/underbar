135cbf06db927e228546a3ad7265adb0b10d557b
sunny-g <sunny.gonna@gmail.com>
2014-09-21 17:44:18 -0500

    finished some*, extend, defaults
    * some could be implemented more cleverly

--- src/underbar.js
+++ src/underbar.js
@@ -221,7 +221,17 @@ var _ = {};
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
-    // TIP: There's a very clever way to re-use every() here.
+    /* simple, non-clever implementation */
+    return _.reduce(collection, function(accumulator, elem) {
+      if (iterator) {
+        return accumulator || !!iterator(elem);
+      } else {
+        return accumulator || !!elem;
+      }
+    }, false);
+
+    // TODO: There's a very clever way to re-use every() here.
+
   };
 
 
@@ -244,11 +254,27 @@ var _ = {};
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    var args = Array.prototype.slice.call(arguments, 1, arguments.length);
+    _.each(args, function(inputObj) {
+      for (var key in inputObj) {
+        obj[key] = inputObj[key];
+      }
+    });
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    var args = Array.prototype.slice.call(arguments, 1, arguments.length);
+    _.each(args, function(inputObj) {
+      for (var key in inputObj) {
+        if (!(key in obj)) {
+          obj[key] = inputObj[key];
+        }
+      }
+    });
+    return obj;
   };
 
 
